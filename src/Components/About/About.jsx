4c84df71ef3a77b7c1ffdd9700d038dd0f8cccdd
Anjali331997 <anjaliap3march1997@gmail.com>
2023-10-04 20:31:24 +0530

    portfolio debugging

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
                 <img src={AboutImg} alt="" className="about__img" />
                 <div className="about__data">
                     {/* <Info /> */}
-                    <p className='about__description'>
+                    <p className='about__description' id='user-detail-intro'>
                         Hi, I'm Anjali A P, MERN stack developer based in Thrissur, Kerala, curious to learn more about developing scalable distributed systems, love problem-solving, and care about writing readable as well as maintainable code.
 <br/><br/>
                         I have extensive experience in various technologies such as Mongo DB, Express, React, and Node. I firmly believe that collaboration is the cornerstone of building great things, and I am always eager to work with others to create something truly amazing.
